fcc39fb0f5ded6d1002d613703a30d45957be7a1
Equal Jungle <93247560+EqualJungle@users.noreply.github.com>
2026-04-09 17:32:51 +1000

    Update index.js navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 // Future functionality goes here
 
 console.log("Site loaded");
+
+function toggleMenu() {
+  const menu = document.getElementById("navLinks");
+  menu.classList.toggle("active");
+}
